dd9e42811d94aeb228c801e185cf5bfbed7deb3c
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2022-02-15 21:50:42 -0300

    update-render-fn

--- src/clj_watson/logic/stdout.clj
+++ src/clj_watson/logic/stdout.clj
@@ -16,13 +16,15 @@
             new-text (format "%s%s[%s]\n" text tabs dependency)]
         (recur new-text (inc count) (next dependencies))))))
 
-(defn ^:private dependencies-hierarchy-to-tree [text]
+(defn ^:private dependencies-hierarchy-to-tree [tree-text dependency-name]
   (fn [render-fn]
-    (when-let [trees (some-> text render-fn edn/read-string)]
-      (->> trees
-           reverse
-           (map dependencies-hierarchy-to-tree*)
-           (reduce #(str %1 "\n" %2))))))
+    (let [trees (some-> tree-text render-fn edn/read-string)]
+      (if (seq trees)
+        (->> trees
+             reverse
+             (map dependencies-hierarchy-to-tree*)
+             (reduce #(str %1 "\n" %2)))
+        (format "[%s]" dependency-name)))))
 
 (defn generate [dependencies template]
   (render template (assoc dependencies :build-tree dependencies-hierarchy-to-tree)))
