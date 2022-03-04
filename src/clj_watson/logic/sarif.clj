4eb21c8985a23005d193bcd7a1e172653881bd57
Matheus Bernardes <12648924+mthbernardes@users.noreply.github.com>
2022-03-04 10:46:50 -0300

    fix output sarif

--- src/clj_watson/logic/sarif.clj
+++ src/clj_watson/logic/sarif.clj
@@ -3,12 +3,12 @@
    [clojure.string :as string]))
 
 (def ^:private sarif-boilerplate
-  {:$schema "https://www.schemastore.org/schemas/json/sarif-2.1.0-rtm.5.json"
-   :version "2.1.0"
+  {:$schema "https://schemastore.azurewebsites.net/schemas/json/sarif-2.1.0-rtm.5.json"
+   :version "3.0.2"
    :runs    [{:tool
               {:driver {:name           "clj-watson"
                         :informationUri "https://github.com/clj-holmes/clj-watson"
-                        :version        "3.0.1"}}}]})
+                        :version        "3.0.2"}}}]})
 
 (defn ^:private advisory->sarif-rule [dependency {{:keys [description summary identifiers cvss]} :advisory}]
   (let [identifier (-> identifiers first :value)]
@@ -16,9 +16,9 @@
       :name                 (format "VulnerableDependency%s" (-> dependency name string/capitalize))
       :shortDescription     {:text summary}
       :fullDescription      {:text description}
-      :help                 {:text (format "Vulnerability found in package %s" dependency)}
+      :help                 {:text (format "Vulnerability found in package %s." dependency)}
       :helpUri              (format "https://github.com/advisories/%s" identifier)
-      :properties           {:security-severity (:score cvss)}
+      :properties           {:security-severity (-> cvss :score str)}
       :defaultConfiguration {:level "error"}}]))
 
 (defn ^:private dependencies->sarif-rules [dependencies]
@@ -29,18 +29,19 @@
                    (reduce concat))))
        (reduce concat)))
 
-(defn ^:private advisory->sarif-result [filename physical-location dependency {{:keys [identifiers]} :advisory}]
+(defn ^:private advisory->sarif-result
+  [filename physical-location remediate-suggestion dependency {{:keys [identifiers]} :advisory}]
   {:ruleId    (-> identifiers first :value)
-   :message   {:text (format "Vulnerability found in package %s" dependency)}
+   :message   {:text (format "Vulnerability found in package %s. Remediate suggestion: %s" dependency remediate-suggestion)}
    :locations [{:physicalLocation
                 {:artifactLocation {:uri filename}
                  :region           physical-location}}]})
 
 (defn ^:private dependencies->sarif-results [dependencies deps-edn-path]
   (->> dependencies
-       (map (fn [{:keys [dependency vulnerabilities physical-location]}]
+       (map (fn [{:keys [dependency vulnerabilities physical-location remediate-suggestion]}]
               (->> vulnerabilities
-                   (map #(advisory->sarif-result deps-edn-path physical-location dependency %)))))
+                   (map #(advisory->sarif-result deps-edn-path physical-location remediate-suggestion dependency %)))))
        (reduce concat)))
 
 (defn generate [dependencies deps-edn-path]
