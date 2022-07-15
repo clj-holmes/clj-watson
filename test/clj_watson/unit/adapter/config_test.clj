698fef9e12bf810523347a7be894752f4cf53c5e
Marko Souza <marko.souza@180s.com.br>
2022-07-15 12:03:32 -0300

    remove matcher-combinators

--- test/clj_watson/unit/adapter/config_test.clj
+++ test/clj_watson/unit/adapter/config_test.clj
@@ -2,19 +2,18 @@
   (:require
    [clj-time.core :as time]
    [clj-watson.adapter.config :as adapter.config]
-   [clojure.test :refer :all]
-   [matcher-combinators.test]))
+   [clojure.test :refer :all]))
 
 (deftest ->allow-config
   (testing "Allow Parsing"
-    (is (match? {"CVE-1234" (time/date-time 2021 5 12)}
+    (is (= {"CVE-1234" (time/date-time 2021 5 12)}
                 (adapter.config/->allow-config {:cve-label "CVE-1234" :expires "2021-05-12"})))
     (is (thrown? IllegalArgumentException
                  (adapter.config/->allow-config {:cve-label "CVE-1234" :expires "wrong date"})))))
 
 (deftest config->allow-list
   (testing "Configs Parsing"
-    (is (match? {"CVE-1234" (time/date-time 2021 5 12)
+    (is (= {"CVE-1234" (time/date-time 2021 5 12)
                  "CVE-5678" (time/date-time 2025 5 12)}
-                (adapter.config/config->allow-config-map [{:cve-label "CVE-1234" :expires "2021-05-12"}
-                                                          {:cve-label "CVE-5678" :expires "2025-05-12"}])))))
+                (adapter.config/config->allow-config-map {:allow-list {:cves [{:cve-label "CVE-1234" :expires "2021-05-12"}
+                                                                              {:cve-label "CVE-5678" :expires "2025-05-12"}]}})))))
