b128fde99ba742306b9152fa7efb34073b3b6166
Marko Souza <marko.souza@180s.com.br>
2022-07-15 12:15:02 -0300

    :nail_care:

--- test/clj_watson/unit/adapter/config_test.clj
+++ test/clj_watson/unit/adapter/config_test.clj
@@ -7,13 +7,13 @@
 (deftest ->allow-config
   (testing "Allow Parsing"
     (is (= {"CVE-1234" (time/date-time 2021 5 12)}
-                (adapter.config/->allow-config {:cve-label "CVE-1234" :expires "2021-05-12"})))
+           (adapter.config/->allow-config {:cve-label "CVE-1234" :expires "2021-05-12"})))
     (is (thrown? IllegalArgumentException
                  (adapter.config/->allow-config {:cve-label "CVE-1234" :expires "wrong date"})))))
 
 (deftest config->allow-list
   (testing "Configs Parsing"
     (is (= {"CVE-1234" (time/date-time 2021 5 12)
-                 "CVE-5678" (time/date-time 2025 5 12)}
-                (adapter.config/config->allow-config-map {:allow-list {:cves [{:cve-label "CVE-1234" :expires "2021-05-12"}
-                                                                              {:cve-label "CVE-5678" :expires "2025-05-12"}]}})))))
+            "CVE-5678" (time/date-time 2025 5 12)}
+           (adapter.config/config->allow-config-map {:allow-list {:cves [{:cve-label "CVE-1234" :expires "2021-05-12"}
+                                                                         {:cve-label "CVE-5678" :expires "2025-05-12"}]}})))))
